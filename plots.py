366cf908c868b142cd50cbea26bd15217a45880a
Fernando Olguin <f.olguin.ch@gmail.com>
2025-04-17 16:34:12 +0900

    Update plots

--- plots.py
+++ plots.py
@@ -9,12 +9,20 @@ parameters.
 """
 from pathlib import Path
 
+from matplotlib import font_manager
 from tile_plotter.plotter import plotter
 
 from common_paths import CONFIGS, FIGURES
 
 plot_type = 'papers'
 #plot_type = 'presentations'
+
+# Import fonts
+font_dirs = [Path('./fonts'), Path('/usr/share/fonts/OTF')]
+font_dirs = filter(lambda x: x.exists(), font_dirs)
+font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
+for font_file in font_files:
+    font_manager.fontManager.addfont(font_file)
 
 if __name__ == '__main__':
     # Constants
@@ -33,6 +41,7 @@ if __name__ == '__main__':
 
     # Read sources from command line
     sources = ['G336.01-0.82']
+    array = 'c8c9'
 
     # Flags
     flags = []
@@ -41,6 +50,7 @@ if __name__ == '__main__':
 
     # Iterate over sources
     for source in sources:
+        plotdir = FIGURES / source / array / plot_type
         # Iterate over steps
         for key, val in steps.items():
             if key in skip:
@@ -49,7 +59,7 @@ if __name__ == '__main__':
             config = config_dir / f'{source}_{val}.cfg'
             if config.exists():
                 print(f'Plotting {config}')
-                plotname = FIGURES / source / plot_type / f'{val}.png'
+                plotname = plotdir / f'{val}.png'
                 plotter([f'{config}', f'{plotname}'] + flags)
             elif itercfgs := config_dir.glob(f'{source}_{val}_*.cfg'):
                 for config in itercfgs:
@@ -57,7 +67,7 @@ if __name__ == '__main__':
                     name = config.stem.split('_')
                     ind = name.index(val.split('_')[0])
                     name = '_'.join(name[ind:])
-                    plotname = FIGURES / source / plot_type / f'{name}.png'
+                    plotname = plotdir / f'{name}.png'
                     plotter([f'{config}', f'{plotname}'] + flags)
             else:
                 print(f'Skipping: {config}')
