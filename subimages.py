7dbafc216fa03df287b3cc1199be64b667a287da
Fernando Olguin <f.olguin.ch@gmail.com>
2025-05-08 12:47:38 +0900

    Change cube size

--- subimages.py
+++ subimages.py
@@ -8,12 +8,12 @@ images = [
     indir / 'G336.018-0.827_spw0_CH3OH_robust2_multiscale.image.fits',
     indir / 'G336.018-0.827_spw2_SO_robust2_multiscale.image.fits',
     indir / 'G336.018-0.827_spw3_SO_robust2_multiscale.image.fits']
-box = '2700,2700,8100,8100'
+box = '3100,3100,7700,7700'
 outdir.mkdir(parents=True, exist_ok=True)
 
 for image in images:
-    outfile = outdir / image.with_suffix('.subim.image').name
+    outfile = outdir / image.with_suffix('.subim').name
     _ = imhead(f'{image}', mode='summary')
     imsubimage(f'{image}', outfile=f'{outfile}', box=box)
-    fitsimage = outfile.with_suffix('.image.fits')
+    fitsimage = outfile.with_suffix('.subim.fits')
     exportfits(imagename=f'{outfile}', fitsimage=f'{fitsimage}')
